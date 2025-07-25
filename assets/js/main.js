5a38d9297e24cb349c2e79684d27665ed7bb5cf5
IsraelAru <israelaruquipaa@gmail.com>
2025-07-24 22:05:32 -0300

    Carrito añadito, falta agregar una funcion para añadir al carrito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,150 +1,3 @@
-/* const texto =
-  "Bienvenido a Indumentaria Coder\n1. Ingresar Articulo\n2. Ver Articulos\n3. Buscar Articulo\n4. Eliminar Articulo\n5. Control de Stock\n6. Salir\n ";
-let bandera = true;
-let seLogeo = false;
-
-const Articulos = [];
-
-class Articulo {
-  constructor(nombre, precio, stock) {
-    this.id = idRand();
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-  }
-}
-
-function idRand() {
-  return Math.floor(Math.random() * 90000) + 10000;
-}
-console.log(idRand());
-
-const creadoraDeArrayDeId = function () {
-  const AuxArray = [];
-
-  for (let i = 0; i < Articulos.length; i++) {
-    AuxArray.push(Articulos[i].id);
-  }
-
-  return AuxArray;
-};
-
-function ingresarArticulo(nombre, precio, stock) {
-  let objetoAuxiliar = new Articulo(nombre, precio, stock);
-
-  Articulos.push(objetoAuxiliar);
-}
-
-function verArticulos() {
-  let mensaje = "";
-  if (Articulos.length == 0) {
-    alert("No se ingreso ningun articulo:");
-    return;
-  }
-  for (let i = 0; i < Articulos.length; i++) {
-    mensaje += `ID: ${Articulos[i].id}\nNombre del articulo: ${Articulos[i].nombre}\nPrecio del articulo: $${Articulos[i].precio}\nStock del articulo: ${Articulos[i].stock}\n`;
-  }
-  alert(mensaje);
-}
-
-function buscadoraPorId(id) {
-  if (Articulos.length == 0) {
-    alert("No se ingreso ningun articulo:");
-    return;
-  }
-  const auxArticulo = Articulos.find((Articulos) => Articulos.id === id);
-
-  if (auxArticulo == undefined) {
-    alert("ID ingresado incorrectamente!");
-  } else {
-    alert(
-      `El articulo buscado es:\nID: ${auxArticulo.id}\nNombre del articulo: ${auxArticulo.nombre}\nPrecio del articulo: $${auxArticulo.precio}\nStock del articulo: ${auxArticulo.stock}\n`
-    );
-  }
-}
-
-function eliminarArticulo(id) {
-  if (Articulos.length == 0) {
-    alert("No se ingreso ningun articulo:");
-    return;
-  }
-  const AuxArray = creadoraDeArrayDeId();
-  let index = AuxArray.indexOf(id);
-  if (index == -1) {
-    alert("ID ingresado incorrectamente!");
-  } else {
-    Articulos.splice(index, 1);
-  }
-}
-
-function controlDeStock(id, stock) {
-  if (Articulos.length == 0) {
-    alert("No se ingreso ningun articulo:");
-    return;
-  }
-  const AuxArray = creadoraDeArrayDeId();
-  let index = AuxArray.indexOf(id);
-  if (index == -1) {
-    alert("ID ingresado incorrectamente!");
-  } else {
-    Articulos[index].stock += stock;
-  }
-}
-
-for (let i = 0; i < 3; i++) {
-  let pedirUsuario = prompt("Ingrese usuario");
-  let pedirContraseña = prompt("Ingrese contraseña");
-
-  ingresoUsuario(pedirUsuario, pedirContraseña);
-  if (seLogeo == true) {
-    break;
-  }
-  if (i == 2) {
-    let confirmacion = confirm("¿Quiere seguir intentando?");
-    if (confirmacion) {
-      i = 0;
-    }
-  }
-}
-if (seLogeo) {
-  bandera = true;
-  while (bandera) {
-    let menu = Number(prompt(texto));
-
-    while (isNaN(menu)) {
-      alert("Ingrese numeros!");
-      menu = Number(prompt(texto));
-    }
-    switch (menu) {
-      case 1:
-        let nombre = prompt("Ingrese el nombre del articulo");
-        let precio = prompt("Ingrese el precio");
-        let stock = Number(prompt("Ingrese el stock"));
-        ingresarArticulo(nombre, precio, stock);
-        break;
-      case 2:
-        verArticulos();
-        break;
-      case 3:
-        let idAux = Number(prompt("Ingrese el ID a buscar"));
-        buscadoraPorId(idAux);
-        break;
-      case 4:
-        let idAux2 = Number(prompt("Ingrese el ID a eliminar"));
-        eliminarArticulo(idAux2);
-        break;
-      case 5:
-        let idAux3 = Number(prompt("Ingrese el ID a buscar"));
-        let auxStock = Number(prompt("Ingrese el stock a agregar/quitar"));
-        controlDeStock(idAux3, auxStock);
-        break;
-      case 6:
-        bandera = false;
-        break;
-    }
-  }
-}
- */
 const body = document.getElementById("products");
 const botonProducto = document.getElementById("agregarProducto");
 const botonDeAgregarProducto = document.getElementById("botonAgregar");
@@ -153,7 +6,9 @@ const nombreProducto = document.getElementById("nombre");
 const imagenProducto = document.getElementById("image");
 const precioProducto = document.getElementById("precio");
 const stockProducto = document.getElementById("stock");
+const cardContenido = document.getElementById("cart-container");
 
+const Carrito = [];
 class Articulo {
   constructor(nombre, image, precio, stock) {
     this.id = idRand();
@@ -165,6 +20,7 @@ class Articulo {
 }
 
 function idRand() {
+  //Funcion para generar un id aleatorio
   return Math.floor(Math.random() * 90000) + 10000;
 }
 
@@ -216,13 +72,43 @@ function dadoraDeEventosABoton() {
   ArrayBotones.forEach((boton) => {
     boton.addEventListener("click", (e) => {
       let producto = buscadoraDeProductos(e.target.id);
-      console.log(producto);
+      Swal.fire({
+        title: "¡Se añadio al carrito!",
+        text: producto.nombre,
+        imageUrl: producto.image,
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Custom image",
+      });
+      agregadoraACarrito(producto);
     });
   });
 }
 
 function buscadoraDeProductos(id) {
   return listaProductos.find((el) => el.id == id);
+}
+
+function agregadoraACarrito(producto) {
+  Carrito.push(producto);
+  creadoraDeCardsDeCarrito();
+}
+function creadoraDeCardsDeCarrito() {
+  Carrito.forEach((producto) => {
+    cardContenido.innerHTML += `
+    <div class="card" style="width: 18rem">
+      <img src="${producto.image}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">
+          $${producto.precio}
+        </p>
+        <p class="card-text">
+          ID:${producto.id}
+        </p>
+      </div>
+    </div>`;
+  });
 }
 if (!seLogeo) {
   alert("¡Debe iniciar sesión!");
@@ -260,7 +146,7 @@ let listaProductos = [
   },
 ];
 
-localStorage.setItem("productos", JSON.stringify(listaProductos));
+localStorage.setItem("products", JSON.stringify(listaProductos));
 
 creadoraDeCard();
 
@@ -292,4 +178,3 @@ botonDeAgregarProducto.onclick = (e) => {
 };
 
 dadoraDeEventosABoton();
-const Carrito = [];
